31effccef923ef32b210e5ccda7ab26d84255933
Peijin Zhang <peijin@ustc.edu>
2025-11-08 03:30:19 +0000

    gen new schem

--- example/create_training_set.py
+++ example/create_training_set.py
@@ -25,8 +25,8 @@ from multiprocessing import Pool, cpu_count
 
 gc.collect()
 
-dir_labels = '/data07/peijinz/ML/type3MLgen/dataset_v3/labels/'
-dir_images = '/data07/peijinz/ML/type3MLgen/dataset_v3/images/'
+dir_labels = '/data07/peijinz/ML/type3MLgen/dataset_v4/labels/'
+dir_images = '/data07/peijinz/ML/type3MLgen/dataset_v4/images/'
 
 # create directories if not exist
 os.makedirs(dir_labels, exist_ok=True)
@@ -37,15 +37,12 @@ totalset = 50000
 
 
 t_burst_hash, f_ax, v_ax = create_radio_burst_hash_table(
-    freq_range=[15, 85], 
+    freq_range=[20, 90], 
     N_freq=640, 
     v_range=[0.04, 0.5], 
     N_v=200)
 
-
 bgfile = '/data07/peijinz/ML/type3MLgen/PhySynthTrainer/physynthtrainer/data/latest_data.3.npz'
-
-
 bg_lst_npz  = "/data07/peijinz/ML/type3MLgen/PhySynthTrainer/data/bckgrd_list.npz"
 
 bgdata = np.load(bgfile)
@@ -60,34 +57,39 @@ def process_single_burst(args):
     
     # Set random seed for this worker to ensure different random values
     np.random.seed(None)
-    
     fname = f'b{i:05d}'
     
-    num_bursts = np.random.randint(3, 40)
-    
-    max_norm_factor = np.random.uniform(2, 145)
-    
-    
-    img_bursts, bursts, is_t3b = generate_many_random_t3_bursts(
+    num_bursts = np.random.randint(10, 80)
+
+    img_bursts, bursts, is_t3b,peak_value = generate_many_random_t3_bursts(
         n_bursts=num_bursts, 
         use_hash_table=True, 
-        freq_range=[22, 75],
+        freq_range=[20, 85],
         hash_table=t_burst_hash, 
         v_hash=v_ax,
         t_res=0.5,
-        t_start=0.0
+        t_start=0.0,
+        flux_max=3000,
+        flux_min=0.3,
+        alpha=1.6
     )
     
-    img_bursts = (img_bursts)/np.max(img_bursts) * max_norm_factor+0.2 # 0.2 is the background level
     
     idx_bg_rand = np.random.randint(0, noise_bg.shape[0])
     noise_bg_arr = resize(np.tile(noise_bg[idx_bg_rand], (10, 1)), img_bursts.shape, order=1)
     img_bursts_withbg = img_bursts+noise_bg_arr
     
+    threshold_sfu = 0.8
+
+    bursts = np.array(bursts)
+    is_t3b = np.array(is_t3b)
+
+    bursts_export = bursts[np.where(np.array(peak_value) > threshold_sfu)[0]].tolist()
+    is_t3b_export = is_t3b[np.where(np.array(peak_value) > threshold_sfu)[0]].tolist()
     
     label_file = export_yolo_label(
-        bursts, 
-        is_t3b, 
+        bursts_export, 
+        is_t3b_export, 
         output_dir=dir_labels, 
         base_filename=fname)
     
@@ -112,7 +114,7 @@ if __name__ == '__main__':
     args_list = [(i, t_burst_hash, v_ax, noise_bg) for i in range(totalset)]
     
     # Use multiprocessing with all available CPU cores
-    num_processes = cpu_count()
+    num_processes = 8#cpu_count()
     print(f"Using {num_processes} CPU cores for parallel processing...")
     
     with Pool(processes=num_processes) as pool:
